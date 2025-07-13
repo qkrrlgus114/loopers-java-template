cd9364e53e72086636da325ec7411a8d8581f913
PARK\qkrrl <qkrrlgus114@naver.com>
2025-07-13 21:13:00 +0900

    feat: 회원가입 단위테스트 작성

---
+++ apps/commerce-api/src/test/java/com/loopers/domain/member/MemberModelTest.java
@@ -0,0 +1,60 @@
+package com.loopers.domain.member;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+public class MemberModelTest {
+
+    @DisplayName("회원가입을 진행할 때, ")
+    @Nested
+    class Register {
+        @DisplayName("ID 가 영문 및 숫자 10자 이내 형식에 맞지 않으면, User 객체 생성에 실패한다.")
+        @Test
+        void failRegister_whenIdNotMatchPattern() {
+            String loginId = "12312321521321";
+            String password = "12341234";
+            String email = "test@naver.com";
+            String name = "박기현";
+            String birth = "1997-12-04";
+
+            assertThatThrownBy(() ->
+                    new MemberModel(loginId, password, email, name, birth))
+                    .isInstanceOf(IllegalArgumentException.class)
+                    .hasMessage("아이디는 [영문 + 숫자] 10자 이하여야 합니다.");
+        }
+
+        @DisplayName("이메일이 xx@yy.zz 형식에 맞지 않으면, User 객체 생성에 실패한다.")
+        @Test
+        void failRegister_whenEmailNotMatchPattern() {
+            String loginId = "test1234";
+            String password = "12341234";
+            String email = "testnaver.com";
+            String name = "박기현";
+            String birth = "1997-12-04";
+
+            assertThatThrownBy(() ->
+                    new MemberModel(loginId, password, email, name, birth))
+                    .isInstanceOf(IllegalArgumentException.class)
+                    .hasMessage("이메일 형식이 일치하지 않습니다.");
+        }
+
+        @DisplayName("생년월일이 yyyy-MM-dd 형식에 맞지 않으면, User 객체 생성에 실패한다.")
+        @Test
+        void failRegister_whenBirthNotMatchPattern() {
+            String loginId = "test1234";
+            String password = "12341234";
+            String email = "test@naver.com";
+            String name = "박기현";
+            String birth = "19927-12-04";
+
+            assertThatThrownBy(() ->
+                    new MemberModel(loginId, password, email, name, birth))
+                    .isInstanceOf(IllegalArgumentException.class)
+                    .hasMessage("생년월일 형식에 문제가 발생했습니다.");
+        }
+    }
+
+}
