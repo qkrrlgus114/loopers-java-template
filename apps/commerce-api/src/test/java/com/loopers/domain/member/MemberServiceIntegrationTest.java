ad0d03d289790ca2c98503ebd2df69660a263cc9
DESKTOP-4DM5HI6\qkrrl <qkrrlgus114@naver.com>
2025-07-15 22:44:04 +0900

    test: 내 정보 조회 통합테스트 추가

--- apps/commerce-api/src/test/java/com/loopers/domain/member/MemberServiceIntegrationTest.java
+++ apps/commerce-api/src/test/java/com/loopers/domain/member/MemberServiceIntegrationTest.java
@@ -1,5 +1,6 @@
 package com.loopers.domain.member;
 
+import com.loopers.application.member.MemberMyInfo;
 import com.loopers.application.member.MemberRegisterInfo;
 import com.loopers.interfaces.api.member.dto.request.MemberRegisterReqDTO;
 import com.loopers.support.error.CoreException;
@@ -11,8 +12,8 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 class MemberServiceIntegrationTest {
@@ -79,5 +80,56 @@ class MemberServiceIntegrationTest {
             });
         }
 
+    }
+
+    @Nested
+    @DisplayName("해당 ID의 회원이 ")
+    class GetMemberInfo {
+
+        /*
+         * - [ ]  해당 ID 의 회원이 존재할 경우, 회원 정보가 반환된다.
+         * - [ ]  해당 ID 의 회원이 존재하지 않을 경우, null 이 반환된다.
+         * */
+
+        @DisplayName("존재할 경우, 회원 정보가 반환된다.")
+        @Test
+        void returnMemberInfo_whenMemberExists() {
+            // given
+            MemberRegisterReqDTO memberRegisterReqDTO = MemberRegisterReqDTO.builder()
+                    .loginId("test")
+                    .password("test")
+                    .email("test@naver.com")
+                    .name("박기현")
+                    .gender("M")
+                    .birth("1997-12-04").build();
+            MemberRegisterInfo saved = memberService.register(memberRegisterReqDTO);
+
+            String memberId = String.valueOf(saved.id());
+            // when
+            MemberMyInfo myMemberInfo = memberService.getMyMemberInfo(memberId);
+
+            // then
+            assertAll(
+                    () -> assertThat(myMemberInfo).isNotNull(),
+                    () -> assertThat(myMemberInfo.loginId()).isEqualTo(memberRegisterReqDTO.getLoginId()),
+                    () -> assertThat(myMemberInfo.email()).isEqualTo(memberRegisterReqDTO.getEmail()),
+                    () -> assertThat(myMemberInfo.name()).isEqualTo(memberRegisterReqDTO.getName()),
+                    () -> assertThat(myMemberInfo.birth()).isEqualTo(memberRegisterReqDTO.getBirth()),
+                    () -> assertThat(myMemberInfo.gender()).isEqualTo(memberRegisterReqDTO.getGender())
+            );
+        }
+
+        @DisplayName("존재하지 않을 경우, null 이 반환된다.")
+        @Test
+        void returnNull_whenMemberDoesNotExist() {
+            // given
+            String memberId = "9999";
+
+            // when
+            MemberMyInfo myMemberInfo = memberService.getMyMemberInfo(memberId);
+
+            // then
+            assertNull(myMemberInfo);
+        }
     }
 }
