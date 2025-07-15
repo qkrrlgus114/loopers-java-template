b2f707f58f3aaa795671130923e24a45534ef6e0
DESKTOP-4DM5HI6\qkrrl <qkrrlgus114@naver.com>
2025-07-16 01:30:35 +0900

    test: 포인트 조회 E2E 테스트 완성

--- apps/commerce-api/src/test/java/com/loopers/interfaces/api/MemberV1ApiE2ETest.java
+++ apps/commerce-api/src/test/java/com/loopers/interfaces/api/MemberV1ApiE2ETest.java
@@ -2,20 +2,15 @@ package com.loopers.interfaces.api;
 
 import com.loopers.interfaces.api.member.dto.request.MemberRegisterReqDTO;
 import com.loopers.interfaces.api.member.dto.response.MemberInfoResDTO;
+import com.loopers.interfaces.api.member.dto.response.MemberPointResDTO;
 import com.loopers.interfaces.api.member.dto.response.MemberRegisterResDTO;
 import com.loopers.utils.DatabaseCleanUp;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Nested;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.core.ParameterizedTypeReference;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
+import org.springframework.http.*;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertAll;
@@ -35,6 +30,20 @@ public class MemberV1ApiE2ETest {
     @Autowired
     private DatabaseCleanUp databaseCleanUp;
 
+    private MemberRegisterReqDTO setUpMemberReqDTO;
+
+    @BeforeEach
+    void setUp() {
+        setUpMemberReqDTO = MemberRegisterReqDTO.builder()
+                .loginId("testUser")
+                .password("testPassword")
+                .email("test@naver.com")
+                .birth("1997-12-04")
+                .name("박기현")
+                .gender("M").build();
+    }
+
+
     @AfterEach
     void tearDown() {
         databaseCleanUp.truncateAllTables();
@@ -47,18 +56,9 @@ public class MemberV1ApiE2ETest {
         @DisplayName("회원 가입이 성공할 경우, 생성된 유저 정보를 응답으로 반환한다.")
         @Test
         void returnMemberRegisterResDTO_whenRegisterMemberSuccessful() {
-            // given
-            MemberRegisterReqDTO reqDTO = MemberRegisterReqDTO.builder()
-                    .loginId("testUser")
-                    .password("testPassword")
-                    .email("test@naver.com")
-                    .birth("1997-12-04")
-                    .name("박기현")
-                    .gender("M").build();
-
             // when
             ResponseEntity<ApiResponse<MemberRegisterResDTO>> response = testRestTemplate.exchange("/api/v1/users", HttpMethod.POST,
-                    new HttpEntity<>(reqDTO), new ParameterizedTypeReference<ApiResponse<MemberRegisterResDTO>>() {
+                    new HttpEntity<>(setUpMemberReqDTO), new ParameterizedTypeReference<ApiResponse<MemberRegisterResDTO>>() {
                     });
 
 
@@ -68,11 +68,11 @@ public class MemberV1ApiE2ETest {
             assertAll(
                     () -> assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK),
                     () -> assertThat(body).isNotNull(),
-                    () -> assertThat(memberRegisterResDTO.getLoginId()).isEqualTo(reqDTO.getLoginId()),
-                    () -> assertThat(memberRegisterResDTO.getEmail()).isEqualTo(reqDTO.getEmail()),
-                    () -> assertThat(memberRegisterResDTO.getName()).isEqualTo(reqDTO.getName()),
-                    () -> assertThat(memberRegisterResDTO.getBirth()).isEqualTo(reqDTO.getBirth()),
-                    () -> assertThat(memberRegisterResDTO.getGender()).isEqualTo(reqDTO.getGender())
+                    () -> assertThat(memberRegisterResDTO.getLoginId()).isEqualTo(setUpMemberReqDTO.getLoginId()),
+                    () -> assertThat(memberRegisterResDTO.getEmail()).isEqualTo(setUpMemberReqDTO.getEmail()),
+                    () -> assertThat(memberRegisterResDTO.getName()).isEqualTo(setUpMemberReqDTO.getName()),
+                    () -> assertThat(memberRegisterResDTO.getBirth()).isEqualTo(setUpMemberReqDTO.getBirth()),
+                    () -> assertThat(memberRegisterResDTO.getGender()).isEqualTo(setUpMemberReqDTO.getGender())
             );
 
         }
@@ -115,15 +115,8 @@ public class MemberV1ApiE2ETest {
         @Test
         void returnMemberInfo_whenGetMyInfoSuccessful() {
             // given
-            MemberRegisterReqDTO reqDTO = MemberRegisterReqDTO.builder()
-                    .loginId("testUser")
-                    .password("testPassword")
-                    .email("test@naver.com")
-                    .birth("1997-12-04")
-                    .name("박기현")
-                    .gender("M").build();
             ResponseEntity<ApiResponse<MemberRegisterResDTO>> registerResponse = testRestTemplate.
-                    exchange("/api/v1/users", HttpMethod.POST, new HttpEntity<>(reqDTO), new ParameterizedTypeReference<>() {
+                    exchange("/api/v1/users", HttpMethod.POST, new HttpEntity<>(setUpMemberReqDTO), new ParameterizedTypeReference<>() {
                     });
 
             // when
@@ -141,11 +134,11 @@ public class MemberV1ApiE2ETest {
                     () -> assertThat(response.getBody()).isNotNull(),
                     () -> {
                         MemberInfoResDTO memberInfo = response.getBody().data();
-                        assertThat(memberInfo.getLoginId()).isEqualTo(reqDTO.getLoginId());
-                        assertThat(memberInfo.getEmail()).isEqualTo(reqDTO.getEmail());
-                        assertThat(memberInfo.getName()).isEqualTo(reqDTO.getName());
-                        assertThat(memberInfo.getBirth()).isEqualTo(reqDTO.getBirth());
-                        assertThat(memberInfo.getGender()).isEqualTo(reqDTO.getGender());
+                        assertThat(memberInfo.getLoginId()).isEqualTo(setUpMemberReqDTO.getLoginId());
+                        assertThat(memberInfo.getEmail()).isEqualTo(setUpMemberReqDTO.getEmail());
+                        assertThat(memberInfo.getName()).isEqualTo(setUpMemberReqDTO.getName());
+                        assertThat(memberInfo.getBirth()).isEqualTo(setUpMemberReqDTO.getBirth());
+                        assertThat(memberInfo.getGender()).isEqualTo(setUpMemberReqDTO.getGender());
                     }
             );
         }
@@ -170,6 +163,57 @@ public class MemberV1ApiE2ETest {
                     () -> assertThat(response.getBody().meta().message()).contains("회원 정보를 찾을 수 없습니다. 회원 ID: " + memberId)
             );
 
+        }
+    }
+
+    @Nested
+    @DisplayName("GET /api/v1/points")
+    class GetMemberPoint {
+
+        /*
+         * - [ ]  포인트 조회에 성공할 경우, 보유 포인트를 응답으로 반환한다.
+         * - [ ]  `X-USER-ID` 헤더가 없을 경우, `400 Bad Request` 응답을 반환한다.
+         */
+
+        @DisplayName("포인트 조회에 성공할 경우, 보유 포인트를 응답으로 반환한다.")
+        @Test
+        void returnMemberPoint_whenGetMemberPointSuccessful() {
+            // given
+            ResponseEntity<ApiResponse<MemberRegisterResDTO>> saved =
+                    testRestTemplate.exchange("/api/v1/users", HttpMethod.POST, new HttpEntity<>(setUpMemberReqDTO),
+                            new ParameterizedTypeReference<>() {
+                            });
+            HttpHeaders headers = new HttpHeaders();
+            headers.set("X-USER-ID", "test");
+
+            // when
+            ResponseEntity<ApiResponse<MemberPointResDTO>> response = testRestTemplate.exchange("/api/v1/points?memberId=" + saved.getBody().data().getId(),
+                    HttpMethod.GET, new HttpEntity<>(headers), new ParameterizedTypeReference<>() {
+                    });
+
+            // then
+            assertAll(
+                    () -> assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK),
+                    () -> assertThat(response.getBody()).isNotNull(),
+                    () -> assertThat(response.getBody().data()).isNotNull(),
+                    () -> assertThat(response.getBody().data().getPoint()).isNotNull()
+            );
+        }
+
+        @DisplayName("`X-USER-ID` 헤더가 없을 경우, `400 Bad Request` 응답을 반환한다.")
+        @Test
+        void returnBadRequest_whenXUserIdHeaderIsMissing() {
+            // when
+            ResponseEntity<ApiResponse<MemberPointResDTO>> response = testRestTemplate.exchange("/api/v1/points",
+                    HttpMethod.GET, null, new ParameterizedTypeReference<>() {
+                    });
+
+            // then
+            assertAll(
+                    () -> assertThat(response.getStatusCode()).isEqualTo(HttpStatus.BAD_REQUEST),
+                    () -> assertThat(response.getBody()).isNotNull(),
+                    () -> assertThat(response.getBody().meta()).isNotNull()
+            );
         }
     }
 }
