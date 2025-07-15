23597f2bc30ee8f536dbc7000410e5938494127c
DESKTOP-4DM5HI6\qkrrl <qkrrlgus114@naver.com>
2025-07-15 22:43:54 +0900

    test: 내 정보 조회 E2E 테스트 추가

--- apps/commerce-api/src/test/java/com/loopers/interfaces/api/MemberV1ApiE2ETest.java
+++ apps/commerce-api/src/test/java/com/loopers/interfaces/api/MemberV1ApiE2ETest.java
@@ -1,7 +1,10 @@
 package com.loopers.interfaces.api;
 
 import com.loopers.interfaces.api.member.dto.request.MemberRegisterReqDTO;
+import com.loopers.interfaces.api.member.dto.response.MemberInfoResDTO;
 import com.loopers.interfaces.api.member.dto.response.MemberRegisterResDTO;
+import com.loopers.utils.DatabaseCleanUp;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
@@ -28,6 +31,14 @@ public class MemberV1ApiE2ETest {
 
     @Autowired
     private TestRestTemplate testRestTemplate;
+
+    @Autowired
+    private DatabaseCleanUp databaseCleanUp;
+
+    @AfterEach
+    void tearDown() {
+        databaseCleanUp.truncateAllTables();
+    }
 
     @DisplayName("POST /api/v1/users")
     @Nested
@@ -88,6 +99,77 @@ public class MemberV1ApiE2ETest {
             assertAll(
                     () -> assertThat(HttpStatus.BAD_REQUEST).isEqualTo(response.getStatusCode())
             );
+        }
+    }
+
+    @Nested
+    @DisplayName("GET /api/v1/users/me")
+    class GetMyInfo {
+
+        /*
+         * - [ ]  내 정보 조회에 성공할 경우, 해당하는 유저 정보를 응답으로 반환한다.
+         * - [ ]  존재하지 않는 ID 로 조회할 경우, `404 Not Found` 응답을 반환한다.
+         * */
+
+        @DisplayName("내 정보 조회에 성공할 경우, 해당하는 유저 정보를 응답으로 반환한다.")
+        @Test
+        void returnMemberInfo_whenGetMyInfoSuccessful() {
+            // given
+            MemberRegisterReqDTO reqDTO = MemberRegisterReqDTO.builder()
+                    .loginId("testUser")
+                    .password("testPassword")
+                    .email("test@naver.com")
+                    .birth("1997-12-04")
+                    .name("박기현")
+                    .gender("M").build();
+            ResponseEntity<ApiResponse<MemberRegisterResDTO>> registerResponse = testRestTemplate.
+                    exchange("/api/v1/users", HttpMethod.POST, new HttpEntity<>(reqDTO), new ParameterizedTypeReference<>() {
+                    });
+
+            // when
+            ResponseEntity<ApiResponse<MemberInfoResDTO>> response = testRestTemplate.exchange(
+                    "/api/v1/users/me?memberId=" + registerResponse.getBody().data().getId(),
+                    HttpMethod.GET,
+                    null,
+                    new ParameterizedTypeReference<>() {
+                    }
+            );
+
+            // then
+            assertAll(
+                    () -> assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK),
+                    () -> assertThat(response.getBody()).isNotNull(),
+                    () -> {
+                        MemberInfoResDTO memberInfo = response.getBody().data();
+                        assertThat(memberInfo.getLoginId()).isEqualTo(reqDTO.getLoginId());
+                        assertThat(memberInfo.getEmail()).isEqualTo(reqDTO.getEmail());
+                        assertThat(memberInfo.getName()).isEqualTo(reqDTO.getName());
+                        assertThat(memberInfo.getBirth()).isEqualTo(reqDTO.getBirth());
+                        assertThat(memberInfo.getGender()).isEqualTo(reqDTO.getGender());
+                    }
+            );
+        }
+
+        @DisplayName("존재하지 않는 ID 로 조회할 경우, `404 Not Found` 응답을 반환한다.")
+        @Test
+        void returnNotFound_whenMemberIdDoesNotExist() {
+            // given
+            String memberId = "9999";
+
+            // when
+            ResponseEntity<ApiResponse<MemberInfoResDTO>> response =
+                    testRestTemplate.exchange("/api/v1/users/me?memberId=" + memberId, HttpMethod.GET, null,
+                            new ParameterizedTypeReference<>() {
+                            });
+
+            // then
+            assertAll(
+                    () -> assertThat(response.getStatusCode()).isEqualTo(HttpStatus.NOT_FOUND),
+                    () -> assertThat(response.getBody()).isNotNull(),
+                    () -> assertThat(response.getBody().meta()).isNotNull(),
+                    () -> assertThat(response.getBody().meta().message()).contains("회원 정보를 찾을 수 없습니다. 회원 ID: " + memberId)
+            );
+
         }
     }
 }
