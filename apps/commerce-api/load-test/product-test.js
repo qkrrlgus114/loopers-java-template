2a1571396c987079c1eac0bfe326060d3a0fb1b0
DESKTOP-4DM5HI6\qkrrl <qkrrlgus114@naver.com>
2025-08-15 05:56:44 +0900

    feat : 레디스 추가

--- apps/commerce-api/load-test/product-test.js
+++ apps/commerce-api/load-test/product-test.js
@@ -2,9 +2,7 @@ import http from 'k6/http';
 
 export const options = {
     stages: [
-        {duration: '30s', target: 1},    // 1명으로 시작
-        {duration: '2m', target: 100},  // 2분 동안 100명까지 증가
-        {duration: '1m', target: 100},  // 1분 유지
+        {duration: '1m', target: 100},  // 1분 동안 100명까지 증가
         {duration: '30s', target: 0},    // 정리
     ],
 };
@@ -13,7 +11,7 @@ const BASE = 'http://localhost:8080';
 
 export default function () {
     const sort = 'LIKE_COUNT_DESC';
-    const page = 999000;
+    const page = 500000;
     const brands = '1';
     const minPrice = 0;
     const maxPrice = 1000000;
