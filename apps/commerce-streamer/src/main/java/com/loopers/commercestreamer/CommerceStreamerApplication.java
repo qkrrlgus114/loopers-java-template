05a1014d7fbda15ce8318d50e8419640ba156b31
DESKTOP-4DM5HI6\qkrrl <qkrrlgus114@naver.com>
2025-09-02 23:12:06 +0900

    feat : 카프카 초기 설정

---
+++ apps/commerce-streamer/src/main/java/com/loopers/commercestreamer/CommerceStreamerApplication.java
@@ -0,0 +1,13 @@
+package com.loopers.commercestreamer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommerceStreamerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CommerceStreamerApplication.class, args);
+	}
+
+}
---
+++ apps/commerce-streamer/src/test/java/com/loopers/commercestreamer/CommerceStreamerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.loopers.commercestreamer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommerceStreamerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
