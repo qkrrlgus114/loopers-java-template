66b7d72d2bdc4312166b72638f07a6c9075fe19d
DESKTOP-4DM5HI6\qkrrl <qkrrlgus114@naver.com>
2025-09-05 02:27:26 +0900

    feat : 메시지 발행 성공, 실패를 로깅

--- apps/commerce-api/src/main/java/com/loopers/application/productlike/producer/ProductLikeProducer.java
+++ apps/commerce-api/src/main/java/com/loopers/application/productlike/producer/ProductLikeProducer.java
@@ -3,20 +3,37 @@ package com.loopers.application.productlike.producer;
 import com.loopers.domain.productlike.event.ProductLikedEvent;
 import com.loopers.domain.productlike.event.ProductUnlikedEvent;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class ProductLikeProducer {
 
     private final KafkaTemplate<String, Object> kafkaTemplate;
 
     public void sendProductLikedEvent(ProductLikedEvent event) {
-        kafkaTemplate.send("product-like-topic", event);
+        kafkaTemplate.send("product-like-topic", event)
+                .whenComplete((result, ex) -> {
+                    if (ex == null) {
+                        log.info("카프카 메시지 전송 성공: {}", result.getRecordMetadata().toString());
+                    } else {
+                        log.error("카프카 메시지 전송 실패: {}", ex.getMessage());
+                    }
+                });
     }
 
     public void sendProdcutUnlikedEvent(ProductUnlikedEvent event) {
-        kafkaTemplate.send("product-unlike-topic", event);
+        kafkaTemplate.send("product-unlike-topic", event)
+                .whenComplete((result, ex) -> {
+                    if (ex == null) {
+                        log.info("카프카 메시지 전송 성공: {}", result.getRecordMetadata().toString());
+                    } else {
+                        log.error("카프카 메시지 전송 실패: {}", ex.getMessage());
+                    }
+                });
+        ;
     }
 }
